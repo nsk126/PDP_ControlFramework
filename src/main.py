bfc481d191a3681893068397e738d7601a7eeead
Suresh <nsk126@gmail.com>
2023-04-11 11:01:25 -0700

    plan

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
 import numpy as np
+
+
+# TODO:
+# 1. Sys Dynamics
+# 2. Control Obj : Cost
+# 3. Trajectories
+# 4. Gradient Descent
+# 5. Auxiliary control sys
+# 6. Chain rule
+# 7. Updare GD
+
+
